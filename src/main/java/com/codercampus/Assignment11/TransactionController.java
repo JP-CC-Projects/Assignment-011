a06dafb89f5048b36505cefe742d82e679fdbb97
JP-CC-Projects <joeparkcc@gmail.com>
2023-09-27 01:59:25 -0500

    cleaned up for submission

--- src/main/java/com/codercampus/Assignment11/TransactionController.java
+++ src/main/java/com/codercampus/Assignment11/TransactionController.java
@@ -19,28 +19,23 @@ public class TransactionController {
     @GetMapping("")
     public String getRootWebPage (ModelMap modelMap){
         modelMap.put("String", new Object());
-        System.out.println("Getmapping 1");
         return "redirect:/transactions";
     }
     @GetMapping("/transactions")
     public String transactionsList(ModelMap modelMap){
         List<Transaction> listOfAllTransactionsByDate = transactionService.listOfAllTransactionsByDate();
         modelMap.put("listOfAllTransactionsByDate", listOfAllTransactionsByDate);
-        System.out.println("Getmapping 2");
         return "transactions";
     }
     @GetMapping("/transactions/{transactionId}")
     public String getSingleTransaction (@PathVariable Long transactionId, ModelMap modelMap){
         Transaction transaction = transactionService.findById(transactionId);
         modelMap.put("transaction", transaction);
-        System.out.println("Getmapping 3");
-        System.out.println(transactionService.findById(transactionId));
         return "singletransaction";
     }
     @PostMapping("/transactions")
     public String postRootWebPage (Transaction transaction){
         Transaction savedTransaction = transactionService.save(transaction);
-        System.out.println("Postmapping 1");
         return "redirect:/transactions";
     }
 }
